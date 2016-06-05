6d9dce2c77c3c6b180eefa3f214d8dd5348f8616
Evgeny Chekalov <evgchekalov@gmail.com>
2016-06-05 21:42:51 +0300

    add initdb.sql

---
+++ src/haw.com/initdb.sql
@@ -0,0 +1,13 @@
+/* very eeasy and not optimized table, only for playing */
+CREATE TABLE listing (
+    uid TEXT,
+    minprice INT,
+    title TEXT,
+    country TEXT,
+    region TEXT,
+    city TEXT,
+    street TEXT,
+    postalcode TEXT,
+    subarea TEXT,
+    housenumber INT,
+    housetype TEXT)
