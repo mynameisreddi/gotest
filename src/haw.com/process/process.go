c268d763bd333a57816108f9bbbdc604d0c8e817
Evgeny Chekalov <evgchekalov@gmail.com>
2016-06-05 19:00:01 +0300

    more userfriendly process

--- src/haw.com/process/process.go
+++ src/haw.com/process/process.go
@@ -1,31 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"haw.com/models"
 	"haw.com/parsers"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	inFile, err := os.Open("/home/reddi/challenge.xml")
+	filename, partner, err := parseArgs()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	defer inFile.Close()
 
-	feed, err := parsers.MakeFeed("xxx")
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	listings, err := parsers.Parse(inFile, feed)
+	defer file.Close()
+
+	feed, err := parsers.MakeFeed(partner)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listings, err := parsers.Parse(file, feed)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for i := range listings {
-		err = models.Save(listings[i])
-		if err != nil {
+		if err = models.Save(listings[i]); err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func parseArgs() (string, string, error) {
+	name := filepath.Base(os.Args[0])
+	if len(os.Args) < 3 {
+		log.Fatalf("%s required 2 args, passed %d", name, len(os.Args)-1)
+	}
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		err := fmt.Errorf("usage %s infile.xml partner", name)
+		return "", "", err
+	}
+
+	return os.Args[1], os.Args[2], nil
 }
